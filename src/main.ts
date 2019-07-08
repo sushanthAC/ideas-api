0791d7dc8c5f168321786839d63a48800463fb97
Sushanth <sush.chikkamath@gmail.com>
2019-07-08 21:59:26 +0530

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import 'dotenv/config';
+import { Logger } from '@nestjs/common';
+
+
+const port = process.env.PORT || 8080;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(port);
+  Logger.log(`Server runing on http://localhost:${port}`, 'Bootstrap');
+}
+bootstrap();
